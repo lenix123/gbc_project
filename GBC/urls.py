d2c7aed5e5e22e9871f75ac3c8d1c17d6808ec54
lenix <lenixreviakin@gmail.com>
2022-05-24 14:29:39 +0800

    Remove node_modules folder

--- GBC/urls.py
+++ GBC/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+
 urlpatterns = [
     path('rest-auth/', include('rest_auth.urls')),
     path('rest-auth/registration/', include('rest_auth.registration.urls')),
